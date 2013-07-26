bd2c107263a4397669f760c63b806d23d928303b
mzabriskie <mzabriskie@gmail.com>
2013-07-26 14:43:39 -0600

    Using a shared template for qunit and speed

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,10 @@
 /*global module:false*/
 module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-include-replace');
     grunt.loadNpmTasks('grunt-testem');
 
     grunt.initConfig({
@@ -13,21 +15,38 @@ module.exports = function(grunt) {
             test_page: 'test/qunit.html'
         },
         uglify: {
-            my_target: {
+            main: {
                 files: {
                     'dist/sickle.min.js': ['src/sickle.js']
                 }
             }
         },
         copy: {
-            main: {
+            dist: {
                 files: [
                     {src: 'src/sickle.js', dest: 'dist/', expand: true, flatten: true}
                 ]
+            },
+            tpl: {
+                files: [
+                    {src: 'test/tpl/*.html', dest: 'test/', expand: true, flatten: true}
+                ]
             }
+        },
+        includereplace: {
+            main: {
+                files: [
+                    {src: 'tpl/qunit.html', dest: 'test/'},
+                    {src: 'tpl/speed.html', dest: 'test/'}
+                ]
+            }
+        },
+        clean: {
+            tpl: 'test/tpl'
         }
     });
 
     grunt.registerTask('default', ['jshint', 'testem']);
-    grunt.registerTask('publish', ['uglify', 'copy']);
+    grunt.registerTask('publish', ['uglify', 'copy:dist']);
+    grunt.registerTask('templates', ['includereplace', 'copy:tpl', 'clean:tpl']);
 };
