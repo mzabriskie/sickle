3e05b9ae53ea37471a49342e94dbc4c3e13551c4
mzabriskie <mzabriskie@gmail.com>
2013-08-13 17:00:14 -0600

    Version bump - 0.1.13

--- dist/sickle.js
+++ dist/sickle.js
@@ -11,7 +11,7 @@
     'use strict';
 
     var Sickle = {
-        version: '0.1.12',
+        version: '0.1.13',
         IGNORE_SYNTAX_ERRORS: true
     };
 
@@ -88,7 +88,7 @@
         return result;
     }
 
-    var RX_ATTR = /\[(.*?)=([^'"]?.*?[^'"]?)\]/g;
+    var RX_ATTR = /\[(.*?)=(.{1}|[^'"]{1}.*?[^'"]{1})\]/g;
     /**
      * Scrub a selector to make it safe for use by Sizzle by adding quotes to attribute expressions
      *
